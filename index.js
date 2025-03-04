b0c518e4b916a5cdbf35104e7ba20df75015784c
alice <alice.nthenge@student.moringaschool.com>
2025-03-04 14:36:58 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code in this file!
+// Write your code on this line!`;
+const currentUser = "Alice";
+
+const welcomeMessage = `Welcome to Flatbook, ${currrentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage. toUpperCase();
+
+const shortGreeting = `Welcome,  ${currentUser .slice(0, 1)}`;
+
+module.exports = {currentUser, welcomeMessage, excitedWelcomeMessage, shortGreeting};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
